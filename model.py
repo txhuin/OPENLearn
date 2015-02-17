a36c3f6b152bee9b46f219c0ac92d1debf8e08f0
Shradha <g.shradha@gmail.com>
2015-02-17 15:36:32 -0800

    Seeding the database

--- model.py
+++ model.py
@@ -12,58 +12,57 @@ session = scoped_session(sessionmaker(bind=engine,
                                       autoflush=False))
 
 Base = declarative_base()
-Base.query = session.query_property
+Base.query = session.query_property()
 
 
-class User(Base):
-	__tablename__ = "users"
+# class User(Base):
+# 	__tablename__ = "users"
 
-	id = Column(Integer, primary_key=True)
-	email = Column(String(64), nullable=False)
-	password = Column(String(64), nullable=False)
+# 	id = Column(Integer, primary_key=True)
+# 	email = Column(String(64), nullable=False)
+# 	password = Column(String(64), nullable=False)
 
 
-class BookmarkedCourse(Base):
-	__tablename__ = "bookmarkedcourses"
+# class BookmarkedCourse(Base):
+# 	__tablename__ = "bookmarkedcourses"
 
-	id = Column(Integer, primary_key=True)
-	user_id = Column(Integer, ForeignKey('users.id'))
-	course = Column(String(500), nullable=False)
+# 	id = Column(Integer, primary_key=True)
+# 	user_id = Column(Integer, ForeignKey('users.id'))
+# 	course = Column(String(500), nullable=False)
 
-	user = relationship("User", backref=backref("bookmarkedcourses", order_by=id))
+# 	user = relationship("User", backref=backref("bookmarkedcourses", order_by=id))
 
 class Course(Base):
 	__tablename__ = "courses"
 
 	id = Column(Integer, primary_key=True)
 	course_shortname = Column(String(200), nullable=False)
-	course_name = Column()
-	# category_id = Column(Integer, ForeignKey("categories.id"), nullable=False)
+	course_name = Column(String(500), nullable=False)
 	course_language = Column(String(200))
 	course_instructor = Column(String(90))
-	course_format = Column()
-	course_workload = Column()
-	course_prerequesites = Column()
-	course_description = Column()
-	course_categories = Column
+	# course_format = Column(String(1000))
+	# course_workload = Column(String)
+	# course_prerequesites = Column(String)
+	# course_description = Column(String)
+	# course_categories = Column
 
 class Term(Base):
 	__tablename__ = "terms"
 
 	id = Column(Integer, primary_key=True)
-	course_id = Column(Integer, ForeignKey()),
+	course_id = Column(Integer, ForeignKey('courses.id'), nullable=False)
 	course_link = Column(String(1000))
-	duration =  Column(Integer, primary_key=True)
-	startDay =  Column(Integer, primary_key=True)
-	startMonth =  Column(Integer, primary_key=True)
-	startYear =  Column(Integer, primary_key=True)		
+	duration =  Column(Integer)
+	startDay =  Column(Integer)
+	startMonth =  Column(Integer)
+	startYear =  Column(Integer)		
 
 class Category(Base):
 	__tablename__ = "categories"
 
 	id = Column(Integer, primary_key=True)
-	category_name = Column
-	category_description = Column()
+	category_name = Column(String(500))
+	category_description = Column(String(5000))
 
 class CourseCategory(Base):
 	__tablename__ = "course_categories"
@@ -74,7 +73,7 @@ class CourseCategory(Base):
 
 
 def main():
-	Base.metadata.create_all(engine)
+	pass
 
 
 if __name__ == "__main__":
--- requestcourse.py
+++ requestcourse.py
@@ -1,11 +1,13 @@
-'''This file is used to make API calls to the Coursera API to retrieve Course and Session information.'''
+'''This file is used to make API calls to the Coursera API to retrieve Course, Term, and Category information.'''
 import requests
 import json
 from datetime import datetime
 import time
 import model
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
-courses = 'https://api.coursera.org/api/catalog.v1/courses?includes=categories,fields=shortName,name,language,instructor,courseFormat, estimatedClassWorkload,recommendedBackground,shortDescription,aboutTheCourse,instructor'
+courses = 'https://api.coursera.org/api/catalog.v1/courses?includes=categories,fields=shortName,name,language,instructor,courseFormat,estimatedClassWorkload,recommendedBackground,shortDescription,aboutTheCourse,instructor'
 terms = 'https://api.coursera.org/api/catalog.v1/sessions?fields=id,courseId,homeLink,durationString,startDay,startMonth,startYear'
 categories = 'https://api.coursera.org/api/catalog.v1/categories?fields=id,name,description'
 
@@ -15,16 +17,16 @@ def get_course():
 	# print r.json()
 	for element in r.json()['elements']:
 		new_course = model.Course(
-		id = element['id'],
-		course_shortname = element['shortName'],
-		course_name = element['name'],
-		course_language = element['language'],
-		course_instructor = element['instructor'],
-		course_format = element['courseFormat'],
-		course_workload = element['estimatedClassWorkload'],
-		course_prerequesites = element['recommendedBackground'],
-		course_description = element['shortDescription'],
-		course_categories = element['links']['categories'])
+			id = element['id'],
+			course_shortname = element['shortName'],
+			course_name = element['name'],
+			course_language = element['language'],
+			course_instructor = element['instructor'],
+			course_format = element['courseFormat'],
+			course_workload = element['estimatedClassWorkload'],
+			course_prerequesites = element['recommendedBackground'],
+			course_description = element['shortDescription'],
+			course_categories = element['links']['categories'])
 
 		model.session.merge(new_course)
 	model.session.commit()
@@ -32,8 +34,13 @@ def get_course():
 def get_term_information():
 
 	t = requests.get(terms)
-	# print t.json()
+	print pp.pprint(t.json())
 	for element in t.json()['elements']:
+		if 'startDay' not in element.keys():
+			print pp.pprint(element)
+			print "This object does not have a startDay"
+			return
+
 		new_term = model.Term(
 			id = element['id'],
 			course_id = element['courseId'],
@@ -49,18 +56,18 @@ def get_term_information():
 def get_category():
 
 	c = requests.get(categories)
-	# print c.json()
+	print c.json()
 
 	for element in c.json()['elements']:
 		new_category = model.Category(
-		id = element['id'],
-		category_name = element['name'],
-		category_description = element['description'])
+			id = element['id'],
+			category_name = element['name'],
+			category_description = element['description'])
 
 		model.session.merge(new_category)
 	model.session.commit()
 
 if __name__ == '__main__':
-	get_course()
+	# get_course()
 	get_term_information()
-	get_category()
+	# get_category()
