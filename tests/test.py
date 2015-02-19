2a38106ac6a27f10a7393d01bfbe2c66dd4ddd49
Shradha <g.shradha@gmail.com>
2015-02-18 20:12:36 -0800

    Tests

---
+++ tests/test.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+def fun(x):
+    return x + 1
+
+class MyTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(fun(3), 4)
---
+++ tests/testing_model.py
@@ -0,0 +1,18 @@
+import unittest
+import model
+
+class Usersignup(unittest.TestCase):
+	"""Tests if a user can sign up"""
+
+	def test_signup:
+	self.assertEqual(model.
+
+
+
+
+
+
+
+
+if __name__ =="__main__":
+	unittest.main()
